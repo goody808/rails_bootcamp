9ec1d31fe60f5cc41a5bf0a25c2e1624d51b364a
goody808 <joshua.gudjohnsen@gmail.com>
2013-06-24 12:52:31 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBootcamp::Application.config.secret_token = '0b6f6082e3baccbd08c6b57a64f75b4c7188a7f22adc77dc8fc52adfb18eac26e2eea7d87f1c8dd75b463b21d3a41a255d27bde0cee88f27684bf2235c586260'
